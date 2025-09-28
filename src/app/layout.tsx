d6637bbfce1483d710cb183a218377d098084e0f
nikoladobros19445-ship-it <nikoladobros19445@gmail.com>
2025-09-29 01:00:16 +0200

    Adding Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import { AuthContextProvider } from '@/context/AuthContext'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: 'Gym Booking',
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Navbar />
             <main className="max-w-4xl mx-auto px-6 py-10">
               {children}
+              <Analytics />
             </main>
           </AuthContextProvider>
         </div>
